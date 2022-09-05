a2321b97f0ce2cb049955084525104286a61d217
acrophobia <916732746@qq.com>
2022-09-05 15:01:06 +0800

    Auth center

--- src/main/java/com/demo/auth/service/AuthService.java
+++ src/main/java/com/demo/auth/service/AuthService.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 /**
  * 使用方式就是自己构造一个对象，控制单例或者不控制as you like
+ * 一些边界条件，包括trim等过于麻烦没有做校验，还有统一异常控制只往上抛了没有做处理
+ * 密码加盐用了最简单的Base64
  */
 public interface AuthService {
     void createUser(String userName, String pass);
